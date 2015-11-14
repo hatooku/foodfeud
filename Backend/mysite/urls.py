d9f814d766bf96008baeb07ee99861f4e93d1ec4
hatooku <jhlee@caltech.edu>
2015-11-14 00:28:39 -0800

    Django Template

---
+++ Backend/mysite/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from mysite.views import *
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'mysite.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^hello/$', hello),
+    url(r'^time/$', current_datetime),
+    url(r'^another-time-page/$', current_datetime),
+    url(r'^time/plus/(\d{1,2})/$', hours_ahead),
+    url(r'^welcome/$', current_url_view_good),
+    url(r'^search-form/$', search_form),
+    url(r'^search/$', search),
+)
